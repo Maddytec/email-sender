563c139fa8ab38af8cb2e8493024ca3181d3afd2
Maddytec <maddytec@gmail.com>
2020-03-05 08:48:15 -0300

    Criação de variaveis de ambiente

--- api/sender.py
+++ api/sender.py
@@ -1,15 +1,21 @@
 import psycopg2
 import redis
 import json
+import os
 from bottle import Bottle, request
+
 
 class Sender(Bottle):
     def __init__(self):
         super().__init__()
         self.route('/', method='POST', callback=self.send)
-        self.fila = redis.StrictRedis(host='queue', port=6379, db=0)
-        DSN = 'dbname=email_sender user=postgres host=db'
-        self.conn = psycopg2.connect(DSN)
+        redis_host = os.getenv('REDIS_HOST', 'queue')
+        self.fila = redis.StrictRedis(host=redis_host, port=6379, db=0)
+        db_name = os.getenv('DB_NAME', 'nome_banco')
+        db_user = os.getenv('DB_USER', 'nome_user')
+        db_host = os.getenv('DB_HOST', 'host_banco')
+        dsn = f'dbname={db_name} user={db_user} host={db_host}'
+        self.conn = psycopg2.connect(dsn)
 
     def register_message(self, assunto, mensagem):
         SQL = 'INSERT INTO emails (assunto, mensagem) VALUES (%s,%s)'
@@ -28,6 +34,7 @@ class Sender(Bottle):
         mensagem = request.forms.get('mensagem')
         self.register_message(assunto, mensagem)
         return 'Mensagem envileirada ! Assunto: {} Mensagem: {}'.format(assunto, mensagem)
+
 
 if __name__ == '__main__':
     sender = Sender()
